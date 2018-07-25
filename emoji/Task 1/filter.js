d0f0cbf49513ad0fa16465548fa5a605ae147210
Bodhish Thomas <bodhish@gmail.com>
2018-07-25 14:53:15 +0530

    Update emoji directory

---
+++ emoji/Task 1/filter.js
@@ -0,0 +1,17 @@
+var emojiList = require("../assets/emojiList.json");
+
+var filterEmoji = function (searchText, maxResults) {
+  return emojiList
+    .filter(emoji => {
+      if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
+        return true;
+      }
+      if (emoji.keywords.includes(searchText)) {
+        return true;
+      }
+      return false;
+    })
+    .slice(0, maxResults);
+}
+
+console.log(filterEmoji("Smile",10))
